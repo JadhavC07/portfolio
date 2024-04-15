c63ab5f40d5a4b1f52ef864e75e69431189369ca
JadhavC07 <cjaddhav@gmail.com>
2024-04-15 11:02:49 +0530

    hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import { useEffect, useState } from "react";
 import Aos from "aos";
+import Home from "./Pages/Home";
+import Skills from "./Pages/Skills";
+import Projects from "./Pages/Projects";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Resume from "./components/Resume";
 
 const Loader = () => {
   return (
@@ -31,15 +37,18 @@ function App() {
       {!isLoading && (
         <>
           <Navbar />
-          <Outlet />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/portfolio/home" element={<Home />} />
+            <Route path="portfolio/skills" element={<Skills />} />
+            <Route path="portfolio/projects" element={<Projects />} />
+            <Route path="/portfolio/about" element={<About />} />
+            <Route path="portfolio/contact" element={<Contact />} />
+            <Route path="portfolio/resume" element={<Resume />} />
+          </Routes>
         </>
       )}
     </div>
-
-    // <>
-    //   <Navbar />
-    //   <Outlet />
-    // </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,57 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
-import About from "./Pages/About.jsx";
-import Skills from "./Pages/Skills.jsx";
-import Resume from "./components/Resume.jsx";
-import Projects from "./Pages/Projects.jsx";
-import Contact from "./Pages/Contact.jsx";
+import { HashRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/portfolio/",
-    element: <App />,
-    children: [
-      {
-        path: "/portfolio/",
-        element: <Home />,
-      },
-      {
-        path: "/portfolio/home",
-        element: <Home />,
-      },
-      {
-        path: "/portfolio/about",
-        element: <About />,
-      },
-      {
-        path: "/portfolio/skills",
-        element: <Skills />,
-      },
-      {
-        path: "/portfolio/resume",
-        element: <Resume />,
-      },
-      {
-        path: "/portfolio/projects",
-        element: <Projects />,
-      },
-      {
-        path: "/portfolio/contact",
-        element: <Contact />,
-      },
-      {
-        path: "/portfolio/projects",
-        element: <Projects />,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/portfolio/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/portfolio/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/portfolio/home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/portfolio/about",
+//         element: <About />,
+//       },
+//       {
+//         path: "/portfolio/skills",
+//         element: <Skills />,
+//       },
+//       {
+//         path: "/portfolio/resume",
+//         element: <Resume />,
+//       },
+//       {
+//         path: "/portfolio/projects",
+//         element: <Projects />,
+//       },
+//       {
+//         path: "/portfolio/contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "/portfolio/projects",
+//         element: <Projects />,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
